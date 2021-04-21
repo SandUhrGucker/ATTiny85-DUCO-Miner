a6193c6d27ecaa3b8c3870c03dba1d7bf1831163
kickshawprogrammer <kickshawprogrammer@gmail.com>
2021-04-21 13:04:33 -0400

    Delete Attiny85_Miner/build/Attiny85_Miner directory

--- Attiny85_Miner/build/Attiny85_Miner/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-d:\projects\DuinoMiners\Attiny85\Attiny85_Miner\build\Attiny85_Miner\core\abi.cpp.o: \
- C:\Users\Jump\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.3\cores\arduino\abi.cpp
